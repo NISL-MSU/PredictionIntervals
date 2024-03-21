abd22269731928cf7aa47dd9c5ae7553b698cb1e
Giorgio Morales Luna <giorgiomoralesluna@gmail.com>
2024-03-21 14:14:18 -0600

    Added experiments

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='MultiSetSR',
+    name='PredictionIntervals',
     version='0.0.1',
     author='Giorgio Morales - Montana State University',
     author_email='giorgiomoralesluna@gmail.com',
